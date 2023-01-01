873845c10b2bd269db137a6fa3d63290ecd07fd8
Priyanka29-lab <109848191+Priyanka29-lab@users.noreply.github.com>
2023-01-01 16:49:04 +0530

    Add files via upload
    inital commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import './App.css';
+import { Card } from './Components/Card';
+
+
+function App() {
+  return (
+    <div className="App">
+      <h1> With Love 💖 </h1>
+      <Card/>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/Components/Card.js
@@ -0,0 +1,27 @@
+import React from 'react'
+
+export const Card = () => {
+  return (
+    <div><div className="NewYearcard">
+    <div className="cardFront">
+      <h3 className="happy">Happy New Year 2023</h3>
+      <div className="balloons">
+        <div className="balloonOne" />
+        <div className="balloonTwo" />
+        <div className="balloonThree" />
+        <div className="balloonFour" />
+      </div>
+    </div>
+    <div className="cardInside">
+      <h3 className="back">It's a Fantastic Year ahead !!</h3>
+      <p>Dear Friend,</p>
+      <p>
+        Happy New Year !! May this year will filled with lots of love and
+        laughter! May all of your 365 days will be Fantastic.
+      </p>
+      <p className="name">Priyanka</p>
+      <button onTouchMove={TouchEvent}></button>
+    </div>
+  </div></div>
+  )
+}
